6ca17dcd4910e291f4a52c9bf914d9d9952ae042
Bunmi Mise <bunmi_mise@yahoo.com>
2018-08-20 20:58:51 -1000

    feat:first TODO passed - url is defined and url not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('url defined', function() {
+            for (let feeds of allFeeds){
+                expect(feeds.url).toBeDefined();
+                expect(feeds.url.length).not.toBe(0);
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
